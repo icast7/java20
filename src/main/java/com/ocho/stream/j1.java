faf85a87e8399a3b7241c0385758c56077251e4c
ikaz <icastillejos@gmail.com>
2019-11-30 19:18:35 -0800

    Added stream creation methods

--- src/main/java/com/ocho/stream/j1.java
+++ src/main/java/com/ocho/stream/j1.java
@@ -2,7 +2,15 @@ package com.ocho.stream;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
@@ -29,18 +37,46 @@ public class j1 {
 
     static  Stream<Integer> streamIterated = Stream.iterate(10, n -> n+2).limit(3);
 
-    IntStream intStream = IntStream.range(1, 3);
-    LongStream longStream = LongStream.rangeClosed(1, 5);
+    static IntStream intStream = IntStream.range(1, 3);
+    static  LongStream longClosedStream = LongStream.rangeClosed(1, 5);
 
-    Random random = new Random();
-    DoubleStream doubleStream = random.doubles(3);
-    //https://www.baeldung.com/java-8-streams
+    static Random random = new Random();
+    static DoubleStream doubleStream = random.doubles(3);
 
-    public static void main(String[] args) {
+    static IntStream streamOfChars = "abcdef".chars();
+
+    static Stream<String> streamOfString = Pattern.compile(", ").splitAsStream("a, b, c");
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
         System.out.println("Generated stream:::");
         streamGenerated.forEach(System.out::println);
 
         System.out.println("Iterated stream:::");
         streamIterated.forEach(System.out::println);
+
+        System.out.println("Int Range:");
+        intStream.forEach(System.out::println);
+
+        System.out.println("Long Range Closed: " +
+                longClosedStream.mapToObj(e -> Long.toString(e))
+                        .reduce(", ", String::concat));
+
+        System.out.println("Double Random Stream: " +
+                doubleStream.mapToObj(e-> Double.toString(e))
+                        .reduce("", (s1, s2) -> s1 + s2 + " "));
+
+        System.out.println("IntStream from chars:");
+        streamOfChars.forEach(System.out::println);
+
+        System.out.println("Stream of Strings: " + streamOfString.reduce("",(s1, s2) -> s1 + s2 + ", "));
+
+        URL resource = j1.class.getClassLoader().getResource("myfile.txt");
+        Path path = Paths.get(resource.toURI());
+        Stream<String> streamOfStrings = Files.lines(path);
+        System.out.println("Stream from file:");
+        streamOfStrings.forEach(System.out::print);
+        System.out.println("Stream from file with charset:");
+        Stream<String> streamOfStringsWithCharSet = Files.lines(path, Charset.forName("UTF-8"));
+        streamOfStringsWithCharSet.forEach(System.out::print);
     }
 }
